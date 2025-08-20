55fe64b700592e2a601fff67b9445e8bdc140ea4
dojofullstack <hola@dojofullstack.com>
2025-08-19 20:59:51 -0500

    Agregar archivos JavaScript y HTML para la estructura inicial de la aplicación, incluyendo variables y configuración de scripts.

---
+++ Frontend/javascript/assetss/js/src/app.js
@@ -0,0 +1,36 @@
+
+console.log("load app.js1");
+console.log("load app.js2");
+console.log("load app.js3");
+console.log("load app.js4");
+console.log("load app.js5");
+
+// var, let, const
+var sombreroMagico = "el valor de mi sombrero magico";
+
+console.log(sombreroMagico);
+
+sombreroMagico = "nuevo valor";
+
+console.log(sombreroMagico);
+
+// creando variables con let
+
+let sombreroMagico2 = "el valor de mi sombrero magico 2";
+
+console.log(sombreroMagico2);
+
+sombreroMagico2 = "nuevo valor";
+
+console.log(sombreroMagico2);
+
+
+// creando variables con const
+const API_BASE = "https://api.example.com";
+const API_VERSION = "v1";
+const API_KEY_CHATGPT = "mi_clave_api_10201029019201209109";
+const API_KEY_STRIPE = "mi_clave_api_10201029019201209109121";
+
+// API_BASE = "https://api.nueva.com";
+
+console.log(API_BASE);
---
+++ Frontend/javascript/assetss/js/src/constantes.js
@@ -0,0 +1,8 @@
+// creando variables con const
+const API_BASE = "https://api.example.com";
+const API_VERSION = "v1";
+const API_KEY = "mi_clave_api_10201029019201209109";
+
+// API_BASE = "https://api.nueva.com";
+
+console.log(API_BASE);
